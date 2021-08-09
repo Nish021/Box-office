3b07aa620a7449b9418642f8f2f4d2c4fb699e2c
Nish021 <trisharaj021@gmail.com>
2021-08-09 19:39:39 +0530

    add show page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React from "react";
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-import Starred from './pages/Starred';
-
-
+import Home from "./pages/Home";
+import Starred from "./pages/Starred";
+import Show from "./pages/Show";
 
 function App() {
   return (
     //entry points
     //1. exact={true} can be written as exact only.
     <div>
-    <Switch>
+      <Switch>
+        <Route exact={true} path="/">
+          <Home />
+        </Route>
 
-      <Route exact={true} path="/">
-        <Home/>
-      </Route>
+        <Route exact={true} path="/starred">
+          <Starred />
+        </Route>
 
-      <Route exact={true} path="/starred">  
-        <Starred/> 
-      </Route>
+        <Route exact path="/show/:id">
+          <Show />
+        </Route>
 
-      <Route>
-        This is 404 page
-      </Route>
-    </Switch>
+        <Route>This is 404 page</Route>
+      </Switch>
     </div>
   );
 }
